9ee9333486850ada97e7d3b24af9ad35c9c883d1
Eric Brelsford <ebrelsford@gmail.com>
2013-07-29 10:58:45 -0400

    Provide a default admin

---
+++ external_data_sync/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+
+class DataSourceAdmin(admin.ModelAdmin):
+    actions = ('make_healthy', 'enable', 'disable',)
+    list_display = ('name', 'synchronize_in_progress', 'healthy', 'enabled',
+                    'ordering', 'last_synchronized', 'next_synchronize',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'ordering',
+                       'synchronizer_record',),
+        }),
+        ('Status', {
+            'fields': (('synchronize_in_progress', 'healthy', 'enabled',),),
+        }),
+        ('Synchronization', {
+            'fields': (
+                ('last_synchronized', 'next_synchronize',
+                 'synchronize_frequency',),
+                'batch_size',
+            ),
+        }),
+    )
+
+    def make_healthy(self, request, queryset):
+        queryset.update(healthy=True)
+    make_healthy.short_description = "Mark selected sources as healthy"
+
+    def enable(self, request, queryset):
+        queryset.update(enabled=True)
+    enable.short_description = "Enable sources"
+
+    def disable(self, request, queryset):
+        queryset.update(enabled=False)
+    disable.short_description = "Disable sources"
