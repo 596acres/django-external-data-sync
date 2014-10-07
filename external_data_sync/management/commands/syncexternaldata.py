e38df0ce93b6a024f3842d4ac171f419d47e1dec
Eric Brelsford <ebrelsford@gmail.com>
2014-10-06 22:11:52 -0400

    Use more specific management command
    syncdata is used by django-extensions

--- external_data_sync/management/commands/syncexternaldata.py
+++ external_data_sync/management/commands/syncexternaldata.py
@@ -7,10 +7,10 @@ from ...models import BaseDataSource
 
 
 class Command(BaseCommand):
-    help = 'Synchronize data sources'
+    help = 'Synchronize external data sources'
 
     def handle(self, *args, **options):
-        self.stdout.write('sync: Running syncdata')
+        self.stdout.write('sync: Running syncexternaldata')
 
         for data_source_cls in BaseDataSource.__subclasses__():
             for data_source in data_source_cls.objects.filter(enabled=True):
@@ -23,4 +23,4 @@ class Command(BaseCommand):
                     traceback.print_exc(file=sys.stdout)
                     continue
 
-        self.stdout.write('sync: Done running syncdata')
+        self.stdout.write('sync: Done running syncexternaldata')
